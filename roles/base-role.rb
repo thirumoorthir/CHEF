18b2843de509c303fa5fca945bf5dfcdad637634
Thiru <moorthy978@gmail.com>
2016-10-11 10:40:15 +0530

    commiting after NTP installation

--- roles/base-role.rb
+++ roles/base-role.rb
@@ -2,4 +2,4 @@
 # This is the first role an machine process after first boot up ,
 name "base-role"
 description "Base Server role  for my entire environment "
-run_list "recipe[chef-client]","recipe[MOTD]", "recipe[Thiru-Apache]","recipe[users]"
+run_list "recipe[chef-client]","recipe[MOTD]", "recipe[Thiru-Apache]","recipe[users]", "recipe[ntp]"
